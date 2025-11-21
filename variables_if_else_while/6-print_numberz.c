70909157e1e8659a799dacc524b5a06979cb3eb1
KarimHajiyev <karimhajiyev1@gmail.com>
2025-11-21 13:28:17 +0000

    Task6: Print all single digit numbers using putchar only, no char variable

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0
+ *              without using any variable of type char
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar(i + 48); /* 48 is the ASCII value of '0' */
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
